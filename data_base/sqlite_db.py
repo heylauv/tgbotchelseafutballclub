a6ebe6c4656e9cff849cf0c2d0f8f98ebf5cb6c6
Nikita Demyan <otherworlldly1@gmail.com>
2022-07-25 15:51:47 +0300

    plsworkcorrectbot

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,31 +1,30 @@
-from optparse import Values
-from os import curdir
-import sqlite3 as sq
-from unicodedata import name
+
 from weakref import proxy
 from create_bot import bot
+import psycopg2 as ps
+import  os
 
 def sql_start():
-    global base , cur
-    base= sq.connect('event_cfc.db')
+    global cur , base
+    base= ps.connect (os.environ.get('DATABASE_URL') , sslmode='require')
     cur=base.cursor()
     if base:
         print ('Подключились к базе данных!')
-    base.execute ('CREATE TABLE IF NOT EXISTS events(img TEXT, name TEXT PRIMARY KEY, discruption TEXT)' )
+    base.execute ('CREATE TABLE IF NOT EXISTS eventt(img TEXT, name TEXT PRIMARY KEY, discruption TEXT)' )
     base.cursor()
 
 async def add_sql_command(state):
     async with state.proxy() as data:
-        cur.execute ('INSERT OR REPLACE INTO events VALUES ( ?, ?, ? )', tuple(data.values()))
+        cur.execute ('INSERT OR REPLACE INTO eventt VALUES ( ?, ?, ? )', tuple(data.values()))
         base.commit()
 
 async def sql_read(message):
-    for ret in cur.execute('SELECT * FROM events').fetchall():
+    for ret in cur.execute('SELECT * FROM eventt').fetchall():
         await bot.send_photo(message.from_user.id , ret[0], f'{ret[1]}\nОписание:\n{ret[2]}\nНадеюсь вам понравилось воспоминание!')
 
 async def sql_read2():
-    return cur.execute ('SELECT * FROM events').fetchall()
+    return cur.execute ('SELECT * FROM eventt').fetchall()
 
 async def sql_delete_command(data):
-    cur.execute ('DELETE FROM events WHEN neame == ?', (data,))
+    cur.execute ('DELETE FROM eventt WHEN neame == ?', (data,))
     base.commit()
