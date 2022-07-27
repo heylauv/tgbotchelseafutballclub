3c572409a0b50f1da58fad615647c47e2cc92760
Nikita Demyan <otherworlldly1@gmail.com>
2022-07-28 01:21:49 +0300

    gfsmgsdfdghfdg

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -16,13 +16,21 @@ base= psycopg2.connect (
 cur = base.cursor()
 if base:
     print ('Я подключился!')
-    cur.execute ('CREATE TABLE IF NOT EXISTS memories(img TEXT, name TEXT PRIMARY KEY, discruption TEXT)' )
+    cur.execute (
+    """CREATE TABLE IF NOT EXISTS memories(
+        img varchar (50) NOT NULL,
+        name varchar (100) PRIMARY KEY,
+        discruption varchar (200) NOT NULL);"""
+        )
     base.cursor()
+    base.commit()
 
 async def add_sql_command(state):
     global base , cur
     async with state.proxy() as data:
-        cur.execute ('INSERT INTO memories (img , name , discruption) VALUES ( ?, ?, ? )', tuple(data.values()))
+        cur.execute (
+            """INSERT INTO memories (img , name , discruption) VALUES
+            (' ? ',' ? ',' ? ' ); """ , tuple(data.values()))
         base.commit()
 
 async def sql_read(message):
