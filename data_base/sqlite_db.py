5302dedf38c468c7535c504bbaab794ccc64b874
Nikita Demyan <otherworlldly1@gmail.com>
2022-07-28 13:39:32 +0300

    efewfwef

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -35,12 +35,13 @@ async def add_sql_command(state):
 
 async def sql_read(message):
     global base , cur
-    for ret in cur.execute('SELECT * FROM memories').fetchone():
+    for ret in cur.execute('SELECT * FROM memories'):
         await bot.send_photo(message.from_user.id , ret[0], f'{ret[1]}\nОписание:\n{ret[2]}\nНадеюсь вам понравилось воспоминание!')
+    cur.fetchall()
 
 async def sql_read2():
     global base , cur
-    return cur.execute ('SELECT * FROM memories').fetchone()
+    return cur.execute ('SELECT * FROM memories').fetchall()
 
 async def sql_delete_command(data):
     global base , cur
