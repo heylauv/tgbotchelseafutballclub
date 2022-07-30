f11bafa752f557d0851b78d93ab91d12398a991c
Nikita Demyan <otherworlldly1@gmail.com>
2022-07-30 22:46:37 +0300

    gggdwdwdgd

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -27,7 +27,7 @@ try:
     async def add_sql_command(state):
         async with state.proxy() as data:
                 cur.execute(
-            """INSERT INTO memorydate (imgg , names , discruptionn) VALUES
+            """INSERT INTO memorydate (photo , names , discruptionn) VALUES
             (' ? ',' ? ',' ? '); """ , tuple(data.values()))
 
     async def sql_read(message):
