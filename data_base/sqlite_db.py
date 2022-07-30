5e0e705b35604c58cdb4f7e693a89acfea41a47e
Nikita Demyan <otherworlldly1@gmail.com>
2022-07-30 12:05:59 +0300

    anotheroneversion

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -3,19 +3,19 @@ from create_bot import bot
 import psycopg2
 from data_base.config import host , user , password , database
 
-try:
-    base= psycopg2.connect (
+#try:
+base= psycopg2.connect (
         host=host ,
         user=user ,
         password=password ,
         database=database
     )
-    base.autocommit=True
-    cur = base.cursor()
+base.autocommit=True
+cur = base.cursor()
 
-    async def sql_start():
-        if base:
-            print ('Я подключился!')
+async def sql_start():
+    if base:
+        print ('Я подключился!')
         cur.execute (
         """CREATE TABLE IF NOT EXISTS memories(
         img varchar (50) NOT NULL,
@@ -24,25 +24,25 @@ try:
         )
     base.cursor()
 
-    async def add_sql_command(state):
-        async with state.proxy() as data:
+async def add_sql_command(state):
+    async with state.proxy() as data:
             cur.execute(
-            """INSERT INTO memories VALUES
+            """INSERT INTO memories (img , name , discruption) VALUES
             (' ? ',' ? ',' ? ' ); """ , tuple(data.values()))
 
-    async def sql_read(message):
-        for ret in cur.execute('SELECT * FROM memories') , cur.fetchall():
-            await bot.send_photo(message.from_user.id , ret[0], f'{ret[1]}\nОписание:\n{ret[2]}\nНадеюсь вам понравилось воспоминание!')
+async def sql_read(message):
+    for ret in cur.execute('SELECT * FROM memories') , cur.fetchall():
+        await bot.send_photo(message.from_user.id , ret[0], f'{ret[1]}\nОписание:\n{ret[2]}\nНадеюсь вам понравилось воспоминание!')
 
-    async def sql_read2():
-        return cur.execute ('SELECT * FROM memories') , cur.fetchall()
+async def sql_read2():
+    return cur.execute ('SELECT * FROM memories') , cur.fetchall()
 
 
-    async def sql_delete_command(data):
-        cur.execute ('DELETE FROM memories WHEN name == ?', (data,))
+async def sql_delete_command(data):
+    cur.execute ('DELETE FROM memories WHEN name == ?', (data,))
 
-except Exception as _ex:
-    print ('INFO', _ex)
-finally:
-    if base:
-        print('Включаюсь!')
+#except Exception as _ex:
+ #   print ('INFO', _ex)
+#finally:
+ #   if base:
+   #     print('Включаюсь!')
