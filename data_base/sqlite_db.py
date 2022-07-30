b276b2955ac225a763111c77026e68da0ad3a5a9
Nikita Demyan <otherworlldly1@gmail.com>
2022-07-30 11:50:27 +0300

    anotherone

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -3,50 +3,46 @@ from create_bot import bot
 import psycopg2
 from data_base.config import host , user , password , database
 
+try:
+    base= psycopg2.connect (
+        host=host ,
+        user=user ,
+        password=password ,
+        database=database
+    )
+    base.autocommit=True
+    cur = base.cursor()
 
-base= psycopg2.connect (
-    host=host ,
-    user=user ,
-    password=password ,
-    database=database
-)
-cur = base.cursor()
-
-async def sql_start():
-    if base:
-        print ('Я подключился!')
+    async def sql_start():
+        if base:
+            print ('Я подключился!')
         cur.execute (
-    """CREATE TABLE IF NOT EXISTS memories(
+        """CREATE TABLE IF NOT EXISTS memories(
         img varchar (50) NOT NULL,
         name varchar (100) PRIMARY KEY,
         discruption varchar (200) NOT NULL);"""
-       )
+        )
     base.cursor()
-    base.commit()
 
-async def add_sql_command(state):
-    async with state.proxy() as data:
-        try:
+    async def add_sql_command(state):
+        async with state.proxy() as data:
             cur.execute(
-        """INSERT INTO memories VALUES
-        (' ? ',' ? ',' ? ' ); """ , tuple(data.values()))
-        except:
-            pass
-    base.commit()
+            """INSERT INTO memories VALUES
+            (' ? ',' ? ',' ? ' ); """ , tuple(data.values()))
 
-async def sql_read(message):
-    global base , cur
-    for ret in cur.execute('SELECT * FROM memories'):
-        await bot.send_photo(message.from_user.id , ret[0], f'{ret[1]}\nОписание:\n{ret[2]}\nНадеюсь вам понравилось воспоминание!')
-    cur.fetchall()
-    base.commit()
+    async def sql_read(message):
+        for ret in cur.execute('SELECT * FROM memories') , cur.fetchall():
+            await bot.send_photo(message.from_user.id , ret[0], f'{ret[1]}\nОписание:\n{ret[2]}\nНадеюсь вам понравилось воспоминание!')
 
-async def sql_read2():
-    global base , cur
-    return cur.execute ('SELECT * FROM memories').fetchall()
+    async def sql_read2():
+        return cur.execute ('SELECT * FROM memories') , cur.fetchall()
 
 
-async def sql_delete_command(data):
-    global base , cur
-    cur.execute ('DELETE FROM memories WHEN name == ?', (data,))
-    base.commit()
+    async def sql_delete_command(data):
+        cur.execute ('DELETE FROM memories WHEN name == ?', (data,))
+
+except Exception as _ex:
+    print ('INFO', _ex)
+finally:
+    if base:
+        print('Включаюсь!')
