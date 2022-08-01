8ee098f47b0352eac0f1c5405c18c93da580b931
Nikita Demyan <otherworlldly1@gmail.com>
2022-08-01 13:29:24 +0300

    jsbfgjksdf

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -2,8 +2,8 @@
 from create_bot import bot
 import psycopg2
 from data_base.config import host , user , password , database
-from psycopg2.errorcodes import UNIQUE_VIOLATION
-from psycopg2 import errors
+#from psycopg2.errorcodes import UNIQUE_VIOLATION
+#from psycopg2 import errors
 
 base= psycopg2.connect (
         host=host ,
@@ -18,9 +18,10 @@ async def sql_start():
     if base:
          print ('Я подключился!')
     cur.execute (
-     """CREATE TABLE IF NOT EXISTS memcfc(
+     """CREATE TABLE IF NOT EXISTS memorycfc(
+        rec_id serial primary key,
         photo TEXT  NOT NULL,
-        name varchar (100) PRIMARY KEY,
+        name TEXT NOT NULL,
         discription TEXT  NOT NULL);"""
      )
     base.cursor()
@@ -29,20 +30,20 @@ async def add_sql_command(state):
     #try:
         async with state.proxy() as data:
             cur.execute(
-        """INSERT INTO memcfc (photo , name , discription) VALUES
-        ( ? , ? , ? ); """ , tuple(data.values()))
+        """INSERT INTO memorycfc (rec_id , photo , name , discription) VALUES
+        (?, ?, ?, ?); """ , tuple(data.values()))
     #except errors.lookup(UNIQUE_VIOLATION) as e:
         #print (e)
 
 
 async def sql_read(message):
-    cur.execute('SELECT * FROM memcfc')
+    cur.execute('SELECT * FROM memorycfc')
     for ret in cur.fetchall():
         await bot.send_photo(message.from_user.id , ret[0], f'{ret[1]}\nОписание:\n{ret[2]}\nНадеюсь вам понравилось воспоминание!')
 
 async def sql_read2():
-    return cur.execute ('SELECT * FROM memcfc') , cur.fetchall()
+    return cur.execute ('SELECT * FROM memorycfc') , cur.fetchall()
 
 
 async def sql_delete_command(data):
-    cur.execute ('DELETE FROM memcfc WHEN name == ?', (data,))
+    cur.execute ('DELETE FROM memorycfc WHEN name == ?', (data,))
